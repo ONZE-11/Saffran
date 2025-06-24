f53c0467c81e55477f2ce1faf863a2136d9ed629
e2295368 <mahjoubia509@gmail.com>
2025-06-24 08:08:57 -0400

    ...

--- lib/db.ts
+++ lib/db.ts
@@ -2,6 +2,7 @@ import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
