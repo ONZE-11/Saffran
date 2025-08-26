a45ca7881cc824c16712f03e2d62b9ab59dcefcb
e2295368 <mahjoubia509@gmail.com>
2025-08-26 18:43:29 -0400

    contact panel

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -1,11 +1,11 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-export const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT) || 587,
-  secure: false, // برای پورت 587 باید false باشه
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
+// export const transporter = nodemailer.createTransport({
+//   host: process.env.SMTP_HOST,
+//   port: Number(process.env.SMTP_PORT) || 587,
+//   secure: false, // برای پورت 587 باید false باشه
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS,
+//   },
+// });
