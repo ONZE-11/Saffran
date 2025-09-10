cd4115376104401fc91e759d786a88ae4104780d
e2295368 <mahjoubia509@gmail.com>
2025-09-09 23:17:35 -0400

    vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  images: { unoptimized: true },
-
-  trailingSlash: false,
-
-  async redirects() {
-    return [
-      // هر مسیر → https://www.elororojo.es
-      {
-        source: '/:path*',
-        destination: 'https://www.elororojo.es/:path*',
-        permanent: true, // 301 Permanent Redirect
-      },
-    ];
-  },
-};
-
-export default nextConfig;
+{
+  "redirects"; [
+    {
+      "source": "/(.*)",
+      "destination": "https://www.elororojo.es/$1",
+      "permanent": true
+    }
+  ]
+}
